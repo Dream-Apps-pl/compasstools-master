c80f57a7f8ebdeb9f98267e5ce341b4fdb2aadcf
Piotr Wosinek <piotr_wosinek@hotmail.com>
2023-03-31 00:37:21 +0200

    update

--- lib/compasstools.dart
+++ lib/compasstools.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 class Compasstools{
 
 
-  static Stream<int> _azimuthValue;
+  static Stream<int?>? _azimuthValue;
 
   static const MethodChannel _channel =
   const MethodChannel('checkDeviceSensors');
@@ -13,15 +13,15 @@ class Compasstools{
   static const EventChannel _eventChannel =
   const EventChannel('azimuthStream');
 
-  static Future<int> get checkSensors async {
-    final int haveSensor = await _channel.invokeMethod('getSensors');
+  static Future<int?> get checkSensors async {
+    final int? haveSensor = await _channel.invokeMethod('getSensors');
     return haveSensor;
   }
 
-  static Stream<int> get azimuthStream{
+  static Stream<int?>? get azimuthStream{
     if (_azimuthValue == null)
     {
-      _azimuthValue = _eventChannel.receiveBroadcastStream().map<int>((value)=>value);
+      _azimuthValue = _eventChannel.receiveBroadcastStream().map<int?>((value)=>value);
     }
 
     return _azimuthValue;
